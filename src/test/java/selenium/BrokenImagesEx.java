509d4d59a09d26b4f22dce4da1ab199a54c323ce
Avinash Bommidi <avinash.bommidi93@gmail.com>
2023-06-17 08:59:23 +0530

    Selenium interview questions updated

--- src/test/java/selenium/BrokenImagesEx.java
+++ src/test/java/selenium/BrokenImagesEx.java
@@ -1,4 +1,4 @@
-package seleniumEx;
+package selenium;
 
 
 import java.io.IOException;
--- src/test/java/selenium/ShadowDOMEx.java
+++ src/test/java/selenium/ShadowDOMEx.java
@@ -1,4 +1,4 @@
-package herokuapp;
+package selenium;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
